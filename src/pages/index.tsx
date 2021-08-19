fca552174f8d9fea356001dc0c2eed264862b57a
Renato Costa <arsmcosta@gmail.com>
2021-08-19 19:26:23 -0300

    Ch III Next - Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+
+import styles from './home.module.scss'
 import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello World
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, welcome!</span>
+          <h1>News about <br />the <span>React</span> world.</h1>
+          <p>
+            Get access to all the plubications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
