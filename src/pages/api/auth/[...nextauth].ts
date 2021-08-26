5952ce336f61c6dde6d6d6d3ee2107d7567e70e5
Renato Costa <arsmcosta@gmail.com>
2021-08-26 16:43:28 -0300

    Ch III Next - Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,8 +11,20 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user',
+      scope: "read:user",
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        
+        return true;
+      } catch {
+        return false
+      }
+
+    },
+  },
 });
