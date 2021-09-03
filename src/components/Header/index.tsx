eae119dd77c0e277c97c6ff9711fb95e652d0577
Renato Costa <arsmcosta@gmail.com>
2021-09-03 02:46:30 -0300

    Ch III JAMStack - Navegação no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,23 @@
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+import Link from "next/link";
 
 export function Header() {
-    return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig news" />
-                <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig news" />
+        <nav>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
+        </nav>
 
-                <SignInButton />
-            </div>
-        </header>
-    );
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
